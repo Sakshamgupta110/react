9935b624d487b58212bb90005a46312991f3fa27
Sakshamgupta110 <sakshamgupta110@gmail.com>
2024-11-18 23:16:43 +0530

    router completed

---
+++ 07reactrouter/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
+import Header from './components/header/header'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
